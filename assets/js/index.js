112919e58df299e8e37c895f1b525403d8be0e8b
SusanAbo <s.aboismaeel@gmail.com>
2022-03-31 00:24:43 +0200

    Done

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,3 +27,32 @@ const books = [
     img: "https://eloquentjavascript.net/img/cover.jpg"
   }
 ];
+
+let ulElement = document.querySelector('[data-ul]')
+
+// erstellen new Card , aus Books Objecte 
+// (wohin): wohin soll dieses Card festgestellt wird 
+
+function addBookCard(bookObj , wohin) {
+  // neues Card erstellen :
+  const card = `<div class="card" style="width: 18rem;">
+  <img src="${bookObj.img}" class="card-img-top book-cover" alt="img">
+  <div class="card-body">
+  <h5 class="card-title">${bookObj.title}</h5>
+  <p class="card-text">${bookObj.author}</p>
+  <span class="badge rounded-pill 
+  ${bookObj.alreadyRead ? "bg-success" : "bg-secondary"}">
+  ${bookObj.alreadyRead ? "Read" : "To read"}</span>
+  </div>
+  </div>`
+
+ // Inhalt von (wohin) element in Html ändern ,und das Card zum Inhalt hinzufügen .
+
+  wohin.innerHTML += card ;
+}
+
+// Function auf Books(Objecte) aufrufen und ausfÜhren :
+// wir bekommen 4 verschiedene Cards in einem Platz (ulElement)
+
+books.forEach(bookObj => addBookCard(bookObj , ulElement))
+
